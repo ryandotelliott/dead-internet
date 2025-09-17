c598649c9f520b5d30f6a56add135f21231db571
Ryan Elliott <rselliott99@gmail.com>
2025-09-17 11:35:46 -0400

    Clean up boilerplate; update schema

--- src/convex/schema.ts
+++ src/convex/schema.ts
@@ -1,12 +1,16 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-// The schema is entirely optional.
-// You can delete this file (schema.ts) and the
-// app will continue to work.
-// The schema provides more precise TypeScript types.
 export default defineSchema({
-  numbers: defineTable({
-    value: v.number(),
+  user: defineTable({
+    name: v.string(),
+    email: v.string(),
+  }),
+  email: defineTable({
+    from: v.id("user"),
+    to: v.array(v.id("user")),
+    subject: v.string(),
+    body: v.string(),
+    createdAt: v.number(),
   }),
 });
